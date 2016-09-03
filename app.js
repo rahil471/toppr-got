449d82d1c5aad158a570a11b2ac762b0f890a9a8
Rahil Shaikh <Rahil Shaikh>
2016-09-04 01:00:43 +0530

    removing morgan log

--- app.js
+++ app.js
@@ -2,20 +2,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser'); 
 var fs = require('fs');
-var morgan = require('morgan');
 var db = require('./models/db.js');
 
 if(GLOBAL.SQLpool === undefined){
 	GLOBAL.SQLpool = db.createPool(); //create a global sql pool connection
 }
-
-morgan.token('res', function getId(res) {
-	return res;
-});
-
-var accessLogStream = fs.createWriteStream(__dirname + '/logs/access.log', {flags: 'a'});
-// setup the logger
-app.use(morgan(':req[body] :res[body]', {stream: accessLogStream}));
 
 app.use(bodyParser.json());  
 
